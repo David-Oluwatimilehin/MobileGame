4530c17d3d14b0a87532fa03440f37a449c956b1
David S <124046476+David-Soremekun@users.noreply.github.com>
2023-10-27 00:34:06 +0100

    Edited Vector2f Class

--- ICAMobileGame/app/src/main/java/com/example/icamobilegame/Vector2f.java
+++ ICAMobileGame/app/src/main/java/com/example/icamobilegame/Vector2f.java
@@ -1,30 +1,32 @@
 package com.example.icamobilegame;
 
-import java.util.Vector;
+import android.util.FloatMath;
 
 public class Vector2f extends java.lang.Object
 {
     protected double x,y;
 
+    public static float TO_RADIANS=(1 / 180.0f) * (float) Math.PI;
+    public static float TO_DEGREES=(1 / (float) Math.PI) * 180.0f;
     public Vector2f()
     {
-        x=0;
-        y=0;
+        this.x=0;
+        this.y=0;
     }
 
     public Vector2f(double vecX, double vecY)
     {
-        x=vecX;
-        y=vecY;
+        this.x=vecX;
+        this.y=vecY;
     }
     public Vector2f(Vector2f other)
     {
-        x=other.x;
-        y=other.y;
+        this.x=other.x;
+        this.y=other.y;
     }
     public void Scale(int value) {
-        x *= value;
-        y *= value;
+        this.x *= value;
+        this.y *= value;
     }
     public double Dot(Vector2f other){
         return 0;
@@ -39,14 +41,14 @@ public class Vector2f extends java.lang.Object
         return 0;
     }
     public void Set(double vecX, double vecY){
-        x=vecX;
-        y=vecY;
+        this.x=vecX;
+        this.y=vecY;
     }
     public Vector2f Multiply(Vector2f vec, double scalar)
     {
-        x*=scalar;
-        y*=scalar;
-        return new Vector2f(x,y);
+        this.x*=scalar;
+        this.y*=scalar;
+        return this;
     }
     public static Vector2f Sum(Vector2f a, Vector2f b){
         Vector2f tempVec = new Vector2f(a.x, a.y);
@@ -54,25 +56,57 @@ public class Vector2f extends java.lang.Object
         tempVec.y += b.y;
         return tempVec;
     }
+    public Vector2f Add(Vector2f vec){
+        this.x= vec.x;
+        this.y= vec.y;
+        return this;
+    }
+    public Vector2f Copy(){
+        return new Vector2f(x,y);
+    }
+    public Vector2f Rotate(float angle)
+    {
+        float rad = angle * TO_RADIANS;
+
+        double cos = Math.cos((double) rad);
+        double sin = Math.sin((double) rad);
+
+        float newX= (float) (this.x * (float)cos - this.y * (float)sin);
+        float newY= (float) (this.x * (float)sin + this.y * (float)cos);
+
+        this.x = newX;
+        this.y = newY;
+        return this;
+    }
+    public float Angle(){
+        float angle= (float) Math.atan2(y,x)*TO_DEGREES;
+        if(angle < 0){
+            angle+=360.0f;
+        }
+        return angle;
+    }
     public void Divide(Vector2f vec,long scalar){
-        vec.x/=scalar;
-        vec.y/=scalar;
+
+        if(scalar != 0){
+            this.x/=scalar;
+            this.y/=scalar;
+        }
+
 
     }
     public Vector2f Sub(Vector2f vec){
-        x -= vec.x;
-        y -= vec.y;
-        return new Vector2f(x,y);
+        this.x -= vec.x;
+        this.y -= vec.y;
+        return this;
     }
     public Vector2f Normalize()
     {
-        Vector2f tempVec = new Vector2f(x,y);
-        double magnitude=Math.sqrt((x*x) + (y*y));
+        double magnitude=Math.sqrt((this.x*this.x) + (this.y*this.y));
 
-        x /=magnitude;
-        y /=magnitude;
+        this.x /=magnitude;
+        this.y /=magnitude;
 
-        return tempVec;
+        return this;
     }
 
     public double Length()
@@ -82,7 +116,7 @@ public class Vector2f extends java.lang.Object
     @Override
     public java.lang.String toString(){
         String conVec;
-        conVec="X: "+ x +" Y: "+ y;
+        conVec="X: "+ (float)this.x +" Y: "+ (float)this.y;
         return conVec;
 
     }
