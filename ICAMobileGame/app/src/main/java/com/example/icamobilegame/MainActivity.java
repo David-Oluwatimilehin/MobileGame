cffe9ace671f3e78bbf35f1219eccb302e99c9e5
David S <124046476+David-Soremekun@users.noreply.github.com>
2023-12-06 02:07:06 +0000

    main menu worked on

--- ICAMobileGame/app/src/main/java/com/example/icamobilegame/MainActivity.java
+++ ICAMobileGame/app/src/main/java/com/example/icamobilegame/MainActivity.java
@@ -4,32 +4,68 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button startButton;
     GameView gameView;
+    boolean StartButtonPressed=false;
     DisplayMetrics display= new DisplayMetrics();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindowManager().getDefaultDisplay().getMetrics(display);
-        gameView= new GameView(this, display);
-        setContentView(gameView);
+        setContentView(R.layout.activity_main);
+        startButton=findViewById(R.id.button);
+
+            if(StartButtonPressed){
+                gameView= new GameView(this, display);
+                setContentView(gameView);
+            }
+            startButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    Log.d("MainActivity","Game Started :) ");
+                    StartButtonPressed=true;
+
+                }
+
+
+            });
+
+
+
+
+
+
+
 
     }
+
+
     @Override
     protected void onResume(){
         super.onResume();
-        gameView.resume();
+
+        //gameView.resume();
+
     }
     protected void onPause(){
         super.onPause();
-        gameView.pause();
+
+        //gameView.pause();
+
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        gameView.stop();
+
+        //gameView.stop();
+
+
     }
 }
